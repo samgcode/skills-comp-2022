a463921f6a9720b6df8943b122759e4b4aa608a3
Sam g <samgcode@gmail.com>
2021-12-18 11:57:59 -0700

    create project - add tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="bg-green-500 text-3xl">test!</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
